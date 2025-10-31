0eec1740db7216de397c31f193ff92fd45b8ce17
Fluvia20079668 <20079668@mydbs.ie>
2025-10-31 20:51:13 +0000

    Set default values for Terraform variables
    Added default values for AWS region, VPC CIDR, AMI ID, and instance type.

--- Infra/variables.tf
+++ Infra/variables.tf
@@ -1,26 +1,24 @@
 variable "aws_region" {
-  type        = string
-  description = "AWS region to deploy resources in"
+  type    = string
+  default = "us-east-1"
 }
 
 variable "vpc_cidr" {
-  type        = string
-  description = "CIDR block for the VPC"
+  type    = string
+  default = "10.0.0.0/16"
 }
 
 variable "subnet_count" {
-  type        = number
-  description = "Number of public subnets"
-  default     = 2
+  type    = number
+  default = 2
 }
 
 variable "ami_id" {
-  type        = string
-  description = "AMI ID for EC2 instance"
+  type    = string
+  default = "ami-0c02fb55956c7d316"  # Amazon Linux 2 in us-east-1
 }
 
 variable "instance_type" {
-  type        = string
-  description = "EC2 instance type"
-  default     = "t2.micro"
+  type    = string
+  default = "t3.micro"  # Free Tier eligible
 }

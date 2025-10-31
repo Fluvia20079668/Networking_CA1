8d310f7f90c702be448df249afac55b555e632b0
Fluvia20079668 <20079668@mydbs.ie>
2025-10-31 20:50:35 +0000

    Fix formatting in main.tf for IAM role

--- Infra/main.tf
+++ Infra/main.tf
@@ -41,7 +41,7 @@ data "aws_iam_policy_document" "eks_assume_role" {
 }
 
 resource "aws_iam_role" "eks_cluster_role" {
-  name               = "terraform-eks-cluster-role-1" 
+  name               = "terraform-eks-cluster-role-1"
   assume_role_policy = data.aws_iam_policy_document.eks_assume_role.json
 }
 

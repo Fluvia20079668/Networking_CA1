eca9b03f3c89e451dfbbf304c49f0c886ddb3524
Fluvia20079668 <20079668@mydbs.ie>
2025-10-31 19:23:36 +0000

    Define VPC CIDR and subnet count variables
    Added variables for VPC CIDR block and subnet count.

--- Infra/variables.tf
+++ Infra/variables.tf
@@ -2,5 +2,17 @@
 variable "aws_region" {
   description = "The AWS region to deploy resources in"
   type        = string
-  default     = "eu-north-1"  # You can change this to your desired region
+  default     = "eu-north-1"  # Change to your preferred region
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_count" {
+  description = "Number of public subnets"
+  type        = number
+  default     = 2
 }

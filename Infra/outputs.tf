1ee863bafd8de991dbf3fa1aba5875a31c43070b
Fluvia20079668 <20079668@mydbs.ie>
2025-10-31 20:51:46 +0000

    Remove output variables from outputs.tf
    Removed output variables for ECR URL, EKS Cluster, Public Subnet IDs, EC2 Instance ID, and EC2 Public IP.

--- Infra/outputs.tf
+++ Infra/outputs.tf
@@ -1,24 +1,19 @@
-# ECR Repository URL
 output "ecr_repository_url" {
   value = aws_ecr_repository.app.repository_url
 }
 
-# EKS Cluster Name
 output "eks_cluster_name" {
   value = aws_eks_cluster.eks.name
 }
 
-# Public Subnet IDs
 output "public_subnet_ids" {
   value = aws_subnet.public[*].id
 }
 
-# EC2 Instance ID
 output "instance_id" {
   value = aws_instance.web.id
 }
 
-# EC2 Public IP
 output "instance_public_ip" {
   value = aws_instance.web.public_ip
 }

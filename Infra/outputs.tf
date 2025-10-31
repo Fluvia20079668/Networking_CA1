46897649f274180dfb8db6f3c98b3ca811b82252
Fluvia20079668 <20079668@mydbs.ie>
2025-10-31 21:24:54 +0000

    Modify outputs in outputs.tf
    Removed instance_id and instance_public_ip outputs, added vpc_id output.

--- Infra/outputs.tf
+++ Infra/outputs.tf
@@ -10,10 +10,7 @@ output "public_subnet_ids" {
   value = aws_subnet.public[*].id
 }
 
-output "instance_id" {
-  value = aws_instance.web.id
+output "vpc_id" {
+  value = aws_vpc.main.id
 }
 
-output "instance_public_ip" {
-  value = aws_instance.web.public_ip
-}

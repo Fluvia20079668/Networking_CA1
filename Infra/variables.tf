7d710303dec80e5e011a9af0abf99830fa106bbc
Fluvia20079668 <20079668@mydbs.ie>
2025-10-31 21:24:26 +0000

    Change default AWS region to eu-north-1

--- Infra/variables.tf
+++ Infra/variables.tf
@@ -1,24 +1,4 @@
 variable "aws_region" {
   type    = string
-  default = "us-east-1"
-}
-
-variable "vpc_cidr" {
-  type    = string
-  default = "10.0.0.0/16"
-}
-
-variable "subnet_count" {
-  type    = number
-  default = 2
-}
-
-variable "ami_id" {
-  type    = string
-  default = "ami-0c02fb55956c7d316"  # Amazon Linux 2 in us-east-1
-}
-
-variable "instance_type" {
-  type    = string
-  default = "t3.micro"  # Free Tier eligible
+  default = "eu-north-1"
 }

e87978a78c32e4ce571a32d473021f621273a4f4
Fluvia20079668 <20079668@mydbs.ie>
2025-11-01 12:40:25 +0000

    Update outputs.tf

--- Infra/outputs.tf
+++ Infra/outputs.tf
@@ -1,11 +1,22 @@
 output "ecr_repository_url" {
   description = "The URL of the ECR repository"
   value       = aws_ecr_repository.app.repository_url
+  sensitive   = false
 }
 
 output "eks_cluster_name" {
   description = "The name of the EKS cluster"
   value       = aws_eks_cluster.eks.name
+}
+
+output "eks_cluster_endpoint" {
+  description = "EKS cluster endpoint"
+  value       = aws_eks_cluster.eks.endpoint
+}
+
+output "eks_cluster_certificate" {
+  description = "EKS cluster CA data (base64)"
+  value       = aws_eks_cluster.eks.certificate_authority[0].data
 }
 
 output "public_subnet_ids" {

9790981db09d1b781488550dd235b929a2ed530a
Fluvia20079668 <20079668@mydbs.ie>
2025-11-01 20:46:02 +0000

    Update  region for s3 in main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -40,6 +40,7 @@ data "aws_availability_zones" "available" {}
 resource "aws_subnet" "public" {
   count                   = var.subnet_count
   vpc_id                  = aws_vpc.main.id
+  # Use cidrsubnet with newbits=8 and netnum=count.index to split /16 into /24s (ok for small examples)
   cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
   availability_zone       = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = true

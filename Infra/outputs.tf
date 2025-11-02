a9135ec44c17f67de9e201bb346991e6a7c83022
Fluvia20079668 <20079668@mydbs.ie>
2025-11-02 17:13:53 +0000

    Update outputs.tf

--- Infra/outputs.tf
+++ Infra/outputs.tf
@@ -1,7 +1,15 @@
-output "ecr_repository_url" {
-  value = aws_ecr_repository.app.repository_url
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnets" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnets" {
+  value = aws_subnet.private[*].id
 }
 
 output "eks_cluster_name" {
-  value = aws_eks_cluster.eks.name
+  value = module.eks.cluster_name
 }

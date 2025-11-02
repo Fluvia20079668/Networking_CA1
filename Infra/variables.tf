3ded3edd1a215e29c17cd0a2f2466c6b43f1d9b3
Fluvia20079668 <20079668@mydbs.ie>
2025-11-02 17:18:46 +0000

    Update variables.tf

--- Infra/variables.tf
+++ Infra/variables.tf
@@ -1,28 +1,23 @@
-# AWS region
 variable "aws_region" {
-  description = "The AWS region to deploy resources in"
+  description = "AWS region"
   type        = string
   default     = "us-west-2"
 }
 
-# VPC CIDR
 variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
+  description = "VPC CIDR block"
   type        = string
   default     = "10.0.0.0/16"
 }
 
-# Number of public subnets
 variable "public_subnet_count" {
   description = "Number of public subnets"
   type        = number
   default     = 2
 }
 
-# Number of private subnets
 variable "private_subnet_count" {
   description = "Number of private subnets"
   type        = number
   default     = 2
 }
-

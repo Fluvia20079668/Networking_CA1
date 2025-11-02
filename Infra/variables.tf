c69f58226733fa7742e9f1f13c6f2d906b1fd9c8
Fluvia20079668 <20079668@mydbs.ie>
2025-11-02 18:41:43 +0000

    Update variables.tf

--- Infra/variables.tf
+++ Infra/variables.tf
@@ -1,36 +1,47 @@
-# AWS region
 variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-west-2"
+  default = "us-east-1"
 }
 
-# VPC
 variable "vpc_cidr" {
-  description = "VPC CIDR block"
-  type        = string
-  default     = "10.0.0.0/16"
+  default = "10.0.0.0/16"
 }
 
 variable "public_subnet_count" {
-  description = "Number of public subnets"
-  type        = number
-  default     = 2
+  default = 2
 }
 
 variable "private_subnet_count" {
-  description = "Number of private subnets"
-  type        = number
-  default     = 2
+  default = 2
 }
 
-# EC2 instances
-variable "ami_id" {
-  description = "AMI ID for EC2 instances"
-  type        = string
+variable "cluster_name" {
+  default = "my-eks-cluster"
+}
+
+variable "cluster_version" {
+  default = "1.29"
 }
 
 variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
+  default = "t3.medium"
+}
+
+variable "ami_id" {
+  default = ""  # leave blank for latest Amazon EKS optimized AMI
+}
+
+variable "node_desired_capacity" {
+  default = 2
+}
+
+variable "node_min_capacity" {
+  default = 1
+}
+
+variable "node_max_capacity" {
+  default = 3
+}
+
+variable "environment" {
+  default = "dev"
 }

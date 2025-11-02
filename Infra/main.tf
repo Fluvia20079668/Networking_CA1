0bb8e8b442c850ea21d1d6639c8b287ae5427863
Fluvia20079668 <20079668@mydbs.ie>
2025-11-02 21:30:05 +0000

    ompatible with terraform-aws-modules/eks/aws v17.24.0 and Terraform 1.6.0,

--- Infra/main.tf
+++ Infra/main.tf
@@ -121,7 +121,7 @@ module "eks" {
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
   vpc_id          = aws_vpc.main.id
-  cluster_subnets = concat(aws_subnet.public[*].id, aws_subnet.private[*].id)
+  subnet_ids      = concat(aws_subnet.public[*].id, aws_subnet.private[*].id)
 
   node_groups = {
     default = {

a22fd919a2389bced6fbe3d233d6a200f64ec518
Fluvia20079668 <20079668@mydbs.ie>
2025-11-02 17:52:20 +0000

    declare all variables your modules/resources need

--- Infra/variables.tf
+++ Infra/variables.tf
@@ -1,9 +1,11 @@
+# AWS region
 variable "aws_region" {
   description = "AWS region"
   type        = string
   default     = "us-west-2"
 }
 
+# VPC
 variable "vpc_cidr" {
   description = "VPC CIDR block"
   type        = string
@@ -20,4 +22,15 @@ variable "private_subnet_count" {
   description = "Number of private subnets"
   type        = number
   default     = 2
+}
+
+# EC2 instances
+variable "ami_id" {
+  description = "AMI ID for EC2 instances"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
 }

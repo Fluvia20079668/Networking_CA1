100c2bdcc481e7fd30d39d8b3e4153bad49e6aa2
Fluvia20079668 <20079668@mydbs.ie>
2025-11-02 00:21:35 +0000

    Update backend.tf

--- Infra/backend.tf
+++ Infra/backend.tf
@@ -13,7 +13,7 @@ terraform {
   backend "s3" {
     bucket         = "my-github-actions-terraform-state"
     key            = "infra/terraform.tfstate"
-    region         = "eu-north-1"              # ✅ match your GitHub Actions AWS_REGION
+    region         = "us-west-2"              # ✅ updated to match your S3 + DynamoDB region
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }

6ce062cba4098bf8ac70869b8b273fc051d05b2d
Fluvia20079668 <20079668@mydbs.ie>
2025-11-02 18:46:17 +0000

    Update outputs.tf

--- Infra/outputs.tf
+++ Infra/outputs.tf
@@ -11,5 +11,17 @@ output "private_subnets" {
 }
 
 output "eks_cluster_name" {
-  value = module.eks.cluster_name
+  value = module.eks.this_eks_cluster_name
+}
+
+output "eks_cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "eks_cluster_security_group_id" {
+  value = module.eks.cluster_security_group_id
+}
+
+output "eks_managed_node_group_names" {
+  value = keys(module.eks.eks_managed_node_groups)
 }

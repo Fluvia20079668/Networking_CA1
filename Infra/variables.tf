80e1e49a1d9c0e3ed09ba4e5a34cea21a05a91e0
Fluvia20079668 <20079668@mydbs.ie>
2025-11-02 14:45:28 +0000

    Update variables.tf

--- Infra/variables.tf
+++ Infra/variables.tf
@@ -1,29 +1,19 @@
 variable "aws_region" {
-  description = "AWS region to deploy resources"
-  type        = string
-  default     = "us-west-2"
+  type    = string
+  default = "us-west-2"
 }
 
 variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"
+  type    = string
+  default = "10.0.0.0/16"
 }
 
 variable "subnet_count" {
-  description = "Number of public subnets"
-  type        = number
-  default     = 2
-}
-
-variable "ami_id" {
-  description = "AMI ID for EC2 instances (if needed)"
-  type        = string
-  default     = ""
+  type    = number
+  default = 2
 }
 
 variable "instance_type" {
-  description = "EC2 instance type for node group"
-  type        = string
-  default     = "t3.medium"
+  type    = string
+  default = "t3.medium"
 }
